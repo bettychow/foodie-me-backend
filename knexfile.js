6ee8891c14bf720f4cf80428a921143357826a00
Betty Chow <bettychchow@gmail.com>
2018-05-14 12:38:00 -0700

    make changes to knexfile

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
     client: 'pg',
     connection: `${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`
   },
-  development: {
+  production: {
     client: 'pg',
     connection: 'postgres://sbvbpanwxemnwq:73324f9a1a1af1002decae1756e18e250951005f127e24ad63637719bb1ed141@ec2-75-101-142-91.compute-1.amazonaws.com:5432/d2o3nke6adq33j',
     migrations: {
