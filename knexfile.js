8dbca5b56df51f8af195e7481fdeb589c7f50c3b
Betty Chow <bettychchow@gmail.com>
2018-05-14 11:54:03 -0700

    Add production config

--- knexfile.js
+++ knexfile.js
@@ -6,5 +6,15 @@ module.exports = {
   test: {
     client: 'pg',
     connection: `${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`
+  },
+  development: {
+    client: 'pg',
+    connection: 'postgres://sbvbpanwxemnwq:73324f9a1a1af1002decae1756e18e250951005f127e24ad63637719bb1ed141@ec2-75-101-142-91.compute-1.amazonaws.com:5432/d2o3nke6adq33j',
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
   }
 };
